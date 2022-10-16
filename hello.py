55837bb0ba09446782d4225ca8e106771d61e61a
Walaeddine Bourahla <walaeddinebourahla@hotmail.com>
2022-10-16 13:26:18 +0800

    Initial Create

---
+++ hello.py
@@ -0,0 +1,7 @@
+# Purpose, Say hello!
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('-n', '--name', help='Name to greet')
+args = parser.parse_args()
+print('Hello, ' + args.name + '!')
